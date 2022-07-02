70238a7d2b551c4168af3b24429c90d409670cd4
Nihat <senius.33@gmail.com>
2022-07-02 16:05:01 +0300

    add some configurations in index

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-/**
- * @format
- */
+import 'react-native-gesture-handler';
+import {AppRegistry, Dimensions} from 'react-native';
 
-import {AppRegistry} from 'react-native';
 import App from './App';
+
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+const entireScreenWidth = Dimensions.get('window').width;
+EStyleSheet.build({$rem: entireScreenWidth / 380});
+
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
