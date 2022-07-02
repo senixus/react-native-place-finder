b9dea217d7c6dd8901b0185ef8f9c8220556ca76
Nihat <senius.33@gmail.com>
2022-07-03 01:56:15 +0300

    add google maps

---
+++ src/components/map/Map.tsx
@@ -0,0 +1,83 @@
+import React, {FC, useEffect} from 'react';
+import {View} from 'react-native';
+import ClusterMapView from 'react-native-map-clustering';
+import {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
+import EStyleSheet from 'react-native-extended-stylesheet';
+import {useSelector} from 'react-redux';
+
+// Interfaces
+import {ISearchSelector} from '../../redux/searchSlice';
+import {ISearchItem} from '../../interfaces/search.interface';
+
+// Components
+import AppText from '../common/AppText';
+
+interface IProps {
+  setMarkerItem?: (value: ISearchItem) => void;
+}
+
+const Map: FC<IProps> = ({setMarkerItem}) => {
+  const searchItems = useSelector<ISearchSelector, ISearchItem[]>(
+    state => state.search.searchItems,
+  );
+
+  useEffect(() => {
+    return () => setMarkerItem && setMarkerItem({} as ISearchItem);
+  }, []);
+
+  return (
+    <>
+      <ClusterMapView
+        animationEnabled={false}
+        toolbarEnabled={false}
+        clusterColor="#d82227"
+        clusteringEnabled={true}
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        initialRegion={{
+          latitude: searchItems[0]?.coordinates?.latitude,
+          longitude: searchItems[0]?.coordinates?.longitude,
+          latitudeDelta: 8.5,
+          longitudeDelta: 8.5,
+        }}>
+        {searchItems?.length > 0 &&
+          searchItems?.map(item => (
+            <Marker
+              onPress={() => setMarkerItem && setMarkerItem(item)}
+              key={item.id}
+              coordinate={{
+                latitude: item.coordinates.latitude,
+                longitude: item.coordinates.longitude,
+              }}>
+              <View style={styles.btn}>
+                <AppText text={item.name} style={styles.name} />
+              </View>
+            </Marker>
+          ))}
+      </ClusterMapView>
+    </>
+  );
+};
+
+export default Map;
+
+const styles = EStyleSheet.create({
+  map: {
+    flex: 1,
+  },
+  btn: {
+    backgroundColor: '#d82227',
+    borderWidth: 1,
+    borderColor: '#d82227',
+    borderRadius: 100,
+    height: '35rem',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 15,
+  },
+  name: {
+    color: '#fff',
+    letterSpacing: 0.5,
+    fontSize: '13rem',
+  },
+});
