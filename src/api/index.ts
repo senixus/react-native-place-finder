134a07a8efbb46f4de7640010aeb825617e238ab
Nihat <senius.33@gmail.com>
2022-07-03 18:35:06 +0300

    update api

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.yelp.com/v3',
   headers: {
-    Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
+    Authorization: `Bearer API KEY`,
   },
 });
