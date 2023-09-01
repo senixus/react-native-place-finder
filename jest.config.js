c7be9c9b5e974427335f503b4ed066270e53e977
Nihat <senius.33@gmail.com>
2023-09-01 22:35:58 +0300

    add configuration for testing

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   preset: 'react-native',
+  setupFiles: ['./node_modules/react-native-gesture-handler/jestSetup.js'],
+  /* This configuration is for React Native projects and it includes for setup for
+     React Navigation, React Navigation Stack and React Native Gesture Handler.
+  */
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native' +
+      '|@react-native' +
+      '|react-navigation' +
+      '|@react-navigation/.*' +
+      '|@react-navigation/stack' +
+      '|react-navigation-stack' +
+      '|@react-native-community/.*' +
+      '))',
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
+  // testEnvironment: 'jsdom',
 };
